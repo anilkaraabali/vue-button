928fbf87bc2898ff762c8f23cf0bfab1162f8613
Anıl Karaabalı <anil@Anl-MacBook-Pro.local>
2020-02-29 18:10:16 +0300

    feat(router): added vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    Hi
+    <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Vue } from 'vue-property-decorator';
 
-@Component({
-  components: {
-  },
-})
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {}
+@import '@/styles/main.scss';
+
+#app {
+  width: 100%;
+  height: 100%;
+}
 </style>
